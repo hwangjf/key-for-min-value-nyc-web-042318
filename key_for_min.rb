982e04b0b0ff6efd3407f13505f3b7f97c85f367
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2018-04-18 23:46:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,11 @@ def key_for_min_value(name_hash)
     else
     min_value = name_hash.first[1]
     min_key = name_hash.first[0]
-    name_hash.each do |key, value|
+    name_hash.each { |key, value|
       if value < min_value
         min_value = value
         min_key = key
-      end
+      }
     end
     min_key
   end 
